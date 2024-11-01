3d18ab8e6b1b69b8017118bb7feb3f8fe69bb484
NoName <foobar@example.com>
2024-11-01 17:51:30 +0300

    doma razobraca

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ from jinja2.lexer import integer_re
 app = Flask(__name__)
 with open('candidates.json', 'r', encoding='utf=8') as file:
     cand = json.loads((file.read()))
+
+
 @app.route('/')
 def user():
     return render_template('25.10.2024.html', cand=cand)
-@app.route('/')
-def id():
-
-    if id == cand['id']:
-
-        return render_template('page_id', cand=cand, id=1)
-
-def sk():
-    sk_list = ['7']
 
 
+@app.route('/id/<int:id>')
+def id(id):
+    for user in cand:
+        if id == user['id']:
+            return render_template('page_id.html', cand=cand, id=id)
 
-    return render_template('25.10.2024.html', user=cand)
-app.run(debug = True)
+
+@app.route('/skills/<skills>')
+def skills(skills):
+
+
+    return render_template('skills.html', cand=cand, skills=skills)
+
+
+app.run(debug=True)
